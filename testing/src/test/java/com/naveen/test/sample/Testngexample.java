7ecf79fe1813e74bc191c249c0806648f36b3dc8
naveensubba <naveenkumarks@gmail.com>
2013-10-26 12:13:54 +0530

    added selenium testing project

---
+++ testing/src/test/java/com/naveen/test/sample/Testngexample.java
@@ -0,0 +1,15 @@
+package com.naveen.test.sample;
+
+import org.testng.annotations.Test;
+
+public class Testngexample {
+	
+	
+	
+	@Test
+
+	public void Test()
+	{
+		System.out.println("sample testng annotation");
+	}
+}
